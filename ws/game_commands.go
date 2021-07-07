f7f8914d67aea00c1ed967af3076d1a7c58b8106
IJustDev <panov@royalzsoftware.de>
2021-07-07 23:27:13 +0200

    feat: add startgame, joingame and login command

---
+++ ws/game_commands.go
@@ -0,0 +1,20 @@
+package ws
+
+import cah "github.com/royalzsoftware/cah/src"
+
+type StartGameCommand struct {
+}
+
+func (c *StartGameCommand) Command() string {
+	return "start_game"
+}
+
+func (c *StartGameCommand) Parameters() []string {
+	return []string{}
+}
+
+func (c *StartGameCommand) Execute(params map[string]string) string {
+	d := cah.GetDefaultDeck()
+	g := cah.NewGame(*d)
+	return g.Id
+}
---
+++ ws/player_commands.go
@@ -0,0 +1,35 @@
+package ws
+
+import cah "github.com/royalzsoftware/cah/src"
+
+type LoginCommand struct {
+}
+
+func (c *LoginCommand) Command() string {
+	return "login"
+}
+
+func (c *LoginCommand) Parameters() []string {
+	return []string{"username"}
+}
+
+func (c *LoginCommand) Execute(params map[string]string) string {
+	username := params["username"]
+	p := cah.NewPlayer(username)
+	return p.Id
+}
+
+type JoinGameCommand struct {
+}
+
+func (c *JoinGameCommand) Command() string {
+	return "start_game"
+}
+
+func (c *JoinGameCommand) Parameters() []string {
+	return []string{"gameId"}
+}
+
+func (c *JoinGameCommand) Execute(params map[string]string) string {
+	return ""
+}
